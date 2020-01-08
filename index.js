65dd08da1c7a889bc1614199f2598744fd0a3848
Matthew Peak <matthewpeak@gmail.com>
2020-01-08 16:45:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // add solution here
 function theBeatlesPlay(dude,instrument){
- let x =[]
+ let x =[];
  for(i=0;i<dude.length;i++){
-   x.push(dude[i]+" plays "+instrument[i])
+   x.push(dude[i]+" plays "+instrument[i]);
  }
- return x 
+ return x ;
 }
 
 function johnLennonFacts(facts){
